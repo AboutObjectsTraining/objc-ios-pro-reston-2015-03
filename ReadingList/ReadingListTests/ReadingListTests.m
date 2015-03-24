654d07919ca8f823b92e746bb85fc48baeef121b
Jonathan Lehr <jonathan@aboutobjects.com>
2015-03-24 13:50:33 -0400

    Added unit test to ReadingList project.

--- ReadingList/ReadingListTests/ReadingListTests.m
+++ ReadingList/ReadingListTests/ReadingListTests.m
@@ -1,40 +1,35 @@
-//
-//  ReadingListTests.m
-//  ReadingListTests
-//
-//  Created by Instructor on 3/24/15.
-//  Copyright (c) 2015 About Objects. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
+#import "RELReadingListController.h"
+#import "ReadingList.h"
+#import "Book.h"
+#import "Author.h"
+
+@interface RELReadingListController()
+@property (strong, nonatomic) NSMutableArray *books;
+- (void)loadReadingList;
+@end
 
 @interface ReadingListTests : XCTestCase
-
 @end
 
 @implementation ReadingListTests
 
 - (void)setUp {
     [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+    putchar('\n');
 }
-
 - (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    putchar('\n');
     [super tearDown];
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    XCTAssert(YES, @"Pass");
-}
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+- (void)testCreateDataSource
+{
+    RELReadingListController *controller = [[RELReadingListController alloc] init];
+    [controller loadReadingList];
+    NSLog(@"%@", controller.books);
+    XCTAssertNotNil(controller.books);
 }
 
 @end
